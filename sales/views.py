3726caa4dbda801012edb9ad65f31a549fd175d6
xiaodufei <dufei0206@gmail.com>
2021-09-12 16:45:54 +0200

    v1 (+ read data + filter)

--- sales/views.py
+++ sales/views.py
@@ -17,6 +17,21 @@ def list_customers(request):
     # key 是字段名，value 是 字段值
     qs = Customer.objects.values()
 
+    # 检查url中是否有参数 phone_number
+    # 有 -- 返回筛选值; 没有 -- 返回None
+    # request.GET 对URL分析, output 是一个 类似字典格式的对象，如下
+    # request.GET = {
+    #     'phone_number': '13899998888',
+    #     'qq': '9116801'
+    # }
+    ph = request.GET.get('phone_number', None)
+
+    # 对数据执行 筛选操作:
+    if ph:
+        # Django 会在底层执行数据库查询的SQL语句 加上相应的 where 从句，进行过滤查询
+        # select * from  Customer where phone_number = 13899998888
+        qs = qs.filter(phone_number=ph)
+
     # 定义返回字符串
     retStr = ''
 
